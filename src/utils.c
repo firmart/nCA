ef09084744619eb019e9864c9c853eda02158eae
firmart <firminmartin24@gmail.com>
2018-09-16 18:06:26 +0200

    move utils functions

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 #include "proto.h"
@@ -16,4 +17,52 @@ int dist_euclidian(int x1, int y1, int x2, int y2) {
     double dy = y1 - y2;
     double dist = sqrt(dx*dx + dy*dy);
     return dist;
+}
+
+
+/* Utils functions */
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color) {
+    int length, x, y;
+    float temp;
+
+    if (win == NULL) {
+        win = stdscr;
+    }
+
+    getyx(win, y, x);
+
+    if (startx != 0) {
+        x = startx;
+    }
+
+    if (starty != 0) {
+        y = starty;
+    }
+
+    if (width == 0) {
+        width = 80;
+    }
+
+    length = strlen(string);
+    temp = (width - length) / 2;
+    x = startx + (int)temp;
+    wattron(win, color);
+    mvwprintw(win, y, x, "%s", string);
+    wattroff(win, color);
+    wrefresh(win);
+}
+
+void win_show(WINDOW *win, char *label, int label_color) {
+    int startx, starty, height, width;
+
+    getbegyx(win, starty, startx);
+    getmaxyx(win, height, width);
+
+    box(win, 0, 0);
+    mvwaddch(win, 2, 0, ACS_LTEE);
+    mvwhline(win, 2, 1, ACS_HLINE, width - 2);
+    mvwaddch(win, 2, width - 1, ACS_RTEE);
+
+    print_in_middle(win, 1, 0, width, label, COLOR_PAIR(label_color));
 }
